c52fcba51130de9423114274ce989c61bdde9db1
Daskar <winworf@gmail.com>
2021-11-22 14:56:59 +0200

    first commin on second branch

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,15 @@ let banner1 = document.getElementById('banner_place'),
     closeBut = document.getElementById('close_but');
 closeBut.onclick = function() {
     banner1.style.visibility = 'hidden';
-}
+};
 
-let sdf = dffff => {};
+//----- Login form --------
+
+let formWrap = document.getElementById('formWrap'),
+    logiN = document.getElementById('logiN'),
+    passworD = document.getElementById('passworD'),
+    preWrap = document.getElementById('preWrap');
+
+formWrap.onmouseover = () =>{
+    preWrap.style.display = 'flex';
+};
--- app/jsOld/main.js
+++ app/jsOld/main.js
@@ -5,6 +5,14 @@ var banner1 = document.getElementById('banner_place'),
 
 closeBut.onclick = function () {
   banner1.style.visibility = 'hidden';
-};
+}; //----- Login form --------
 
-var sdf = function sdf(dffff) {};
+
+var formWrap = document.getElementById('formWrap'),
+    logiN = document.getElementById('logiN'),
+    passworD = document.getElementById('passworD'),
+    preWrap = document.getElementById('preWrap');
+
+formWrap.onmouseover = function () {
+  preWrap.style.display = 'flex';
+};
